6567d84b549a052fc192d706cfd450f5a846eafa
0xF48 <poofyiq@gmail.com>
2023-10-20 13:40:20 -0700

    update contract to store multiple hashes

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -2,13 +2,29 @@
 pragma solidity ^0.8.0;
 
 contract GoodpromptRegistry {
-    string private ipfsHash;
-
-    function storeHash(string calldata _ipfsHash) public {
-        ipfsHash = _ipfsHash;
+    struct HashEntry {
+        string ipfsHash;
+        address owner;
     }
 
-    function retrieveHash() public view returns (string memory) {
-        return ipfsHash;
+    HashEntry[] public hashEntries;
+
+    function storeHash(string calldata _ipfsHash) public {
+        HashEntry memory newEntry = HashEntry({
+            ipfsHash: _ipfsHash,
+            owner: msg.sender
+        });
+        hashEntries.push(newEntry);
+    }
+
+    function retrieveHash(
+        uint256 index
+    ) public view returns (string memory, address) {
+        require(index < hashEntries.length, "Index out of bounds");
+        return (hashEntries[index].ipfsHash, hashEntries[index].owner);
+    }
+
+    function getHashCount() public view returns (uint256) {
+        return hashEntries.length;
     }
 }
